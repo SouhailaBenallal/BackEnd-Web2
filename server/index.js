51ff02935f0d29836db5174871ee756231b819c7
SouhailaBenallal <souhaila.ben.allal@student.ehb.be>
2022-07-30 13:09:06 +0200

    Connect to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 import express from "express";
-const port = 3003;
+import mongodb from "mongodb";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+
 
 const app = express();
+app.use(morgan("start"));
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Hello testing");
-});
+const URL_MONGODB = "mongodb+srv://SouhailaBenAllal:RxsloScxMVRe6m2J@cluster0.il0qq.mongodb.net/BEL-universities?retryWrites=true&w=majority"
 
 
-app.listen(port,() => {
-    console.log(`Server listen to port ${port}`)
-})
+const port = 3003;
+
+mongoose.connect(URL_MONGODB)
+  .then(() => {
+    app.listen(port, () => console.log(`Server listen to ${port}`));
+}).catch((error) => console.log(`${error} did not connect`));
